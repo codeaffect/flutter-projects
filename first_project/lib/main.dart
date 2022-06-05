7c4aad94850296eaff731463c35def01ece5b183
codeaffect <sandeepjhankar@gmail.com>
2022-06-05 09:37:07 -0400

    newfirstproject

---
+++ first_project/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Hello There!'),
+          centerTitle: true,
+        ),
+        body: const Text('Body'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
